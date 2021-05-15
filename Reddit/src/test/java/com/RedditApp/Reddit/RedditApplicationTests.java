d91214513efecbfa6b173723d603be26fe29c9e4
Baljinder Singh <baljinder9219@gmail.com>
2021-05-15 22:42:46 +0530

    first commit

---
+++ Reddit/src/test/java/com/RedditApp/Reddit/RedditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RedditApp.Reddit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
